2e04bc1b7fcb2c388f26b531d39329020d676372
Adrian Meza <adrianme213@gmail.com>
2018-02-16 23:49:11 -0800

    [feat] - install axios for querying mock API data - add posts component - ng router redirect to posts

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+// Declare axios for making HTTP requests
+const axios = require('axios');
+const API = 'https://jsonplaceholder.typicode.com';
+
 // Get API Listing
 router.get('/', (req, res) => {
   res.send('API Works');
+});
+
+// Get all posts
+router.get('/posts', (req, res) => {
+  // Grabbing posts from mock API
+  // Later replace with MongoDB
+  axios.get(`${API}/posts`)
+    .then(posts => {
+      res.status(200).json(posts.data);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
 });
 
 module.exports = router;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
+// Define the routes
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent // PostsComponent injected via Angular CLI
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES) // This adds ROUTES to the app
   ],
   providers: [],
   bootstrap: [AppComponent]
